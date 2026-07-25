68fcd5d9b6b14c0f60f45197d1be4a2a55eb215f
Thanakit Chuchoed <tanakit200663@gmail.com>
2026-07-26 00:01:56 +0700

    feat(emr): setup database connection for emr_db

--- his-project/apps/emr-bc/src/emr-bc.module.ts
+++ his-project/apps/emr-bc/src/emr-bc.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmrBcController } from './emr-bc.controller';
 import { EmrBcService } from './emr-bc.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '../.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST', 'localhost'),
+        port: configService.get<number>('POSTGRES_PORT', 5432),
+        username: configService.get<string>('POSTGRES_USERNAME', 'postgres'),
+        password: configService.get<string>('POSTGRES_PASSWORD', 'postgres'),
+        database: configService.get<string>('EMR_DATABASE', 'emr_db'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [EmrBcController],
   providers: [EmrBcService],
 })
