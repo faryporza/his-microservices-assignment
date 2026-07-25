94e17a7eb4ed795b64202f2d5027d3711fc4f530
Thanakit Chuchoed <tanakit200663@gmail.com>
2026-07-25 15:27:40 +0700

    feat(opd): setup database connection for opd_db

--- his-project/apps/opd-bc/src/opd-bc.module.ts
+++ his-project/apps/opd-bc/src/opd-bc.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OpdBcController } from './opd-bc.controller';
 import { OpdBcService } from './opd-bc.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '../.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST', 'localhost'),
+        port: configService.get<number>('POSTGRES_PORT', 5432),
+        username: configService.get<string>('POSTGRES_USERNAME', 'postgres'),
+        password: configService.get<string>('POSTGRES_PASSWORD', 'postgres'),
+        database: configService.get<string>('OPD_DATABASE', 'opd_db'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [OpdBcController],
   providers: [OpdBcService],
 })
